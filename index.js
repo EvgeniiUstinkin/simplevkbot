2a1c9eb0c012515d06eddf23df6387b0683ac201
booomch <jekka1991@ya.ru>
2017-04-04 21:45:23 +0300

    track package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var VK = require("VK-Promise"),
-    https = require("https"),
+    https = require("http"),
     vk = VK("83b9df8a46547ed328b4d47b76e742a7c5d778954b5991a8616c0cbc16432db74b0130f2d768c5136d443");
 
 // Запускаем https сервер на 80 порту с обработкой Callback API
@@ -11,7 +11,7 @@ https.createServer(function (req, res) {
         return callback(req, res);
     // Далее делаем все что нам нужно
     res.end("Error 404");
-}).listen(443);
+}).listen(80);
 
 // Включаем оптимизацию через execute
 // Собирает все запросы и выполняет пачками раз в 334мс
